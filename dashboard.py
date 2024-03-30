b3610b8e9fd037e49bb81bd39d85950f53d3ffaf
agungdiah <121778260+agungdiah@users.noreply.github.com>
2024-03-30 23:28:43 +0800

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -144,7 +144,7 @@ with col2:
     
     st.pyplot(fig)
 
-st.text("Test")
+st.text("Pada tahun 2017 total orders dan total revenue cenderung mengalami peningkatan setiap bulannya, peningkatan tertinggi berada pada bulan November. Namun di bulan Desember total orders dan total revenue mengalami penurunan tajam")
 
 
 # visualisasi produk
