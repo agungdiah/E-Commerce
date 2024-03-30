99cc6f88b41f5c15f8b4667dc06c558bbfa73e4b
agungdiah <121778260+agungdiah@users.noreply.github.com>
2024-03-30 22:56:40 +0800

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
-sns.set(style="dark")
 
 # load data
 data = pd.read_csv("data_main.csv")
